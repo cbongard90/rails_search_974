d338d425f44cc29205c9c82b90a2028627a866d3
Christian Bongard <christian.bongard90@gmail.com>
2022-09-01 09:29:24 +0400

    search with active record

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,12 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:query].present?
+      # @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+
+      sql_query = "title ILIKE :query OR synopsis ILIKE :query"
+      @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
   end
 end
