aea2220d423b137625a20f15aaeaea3a579b05ba
Christian Bongard <christian.bongard90@gmail.com>
2022-09-01 09:39:31 +0400

    completed with plain active record search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,8 +3,23 @@ class MoviesController < ApplicationController
     if params[:query].present?
       # @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
 
-      sql_query = "title ILIKE :query OR synopsis ILIKE :query"
-      @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+      # sql_query = "title ILIKE :query OR synopsis ILIKE :query"
+      # @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+
+      # sql_query = <<~SQL
+      #   movies.title ILIKE :query
+      #   OR movies.synopsis ILIKE :query
+      #   OR directors.first_name ILIKE :query
+      #   OR directors.last_name ILIKE :query
+      # SQL
+
+      sql_query = <<~SQL
+        movies.title @@ :query
+        OR movies.synopsis @@ :query
+        OR directors.first_name @@ :query
+        OR directors.last_name @@ :query
+      SQL
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
     else
       @movies = Movie.all
     end
